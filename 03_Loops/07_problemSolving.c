014e88ec9f271e43fb2a8099b03c8e37dfc595db
HassProgrammer <md.ahshanulalam@gmail.com>
2024-08-13 15:12:57 +0600

    problem solving

---
+++ 03_Loops/07_problemSolving.c
@@ -0,0 +1,13 @@
+// HW : Display this AP - 4,7,10,13,16.. upto 'n'
+// terms.
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number\n: ");
+    scanf("%d",&n);
+    for(int i=1; i<=3*n+1; i=i+3 ){
+        printf("%d ",i);
+    }
+    return 0;
+}
