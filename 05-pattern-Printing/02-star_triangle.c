7ba07d0f1fe2e4210d5a14d88e97f28183681f66
HassProgrammer <md.ahshanulalam@gmail.com>
2025-01-27 10:35:56 +0600

    Print the given pattern star triangle

--- 05-pattern-Printing/02-star_triangle.c
+++ 05-pattern-Printing/02-star_triangle.c
@@ -12,7 +12,7 @@ int main(){
     for(int i=1; i<=n; i++){
         printf("\n");
         for(int j=1; j<=i; j++){
-            printf("*");
+            printf("* ");
         }
     }
     return 0;
